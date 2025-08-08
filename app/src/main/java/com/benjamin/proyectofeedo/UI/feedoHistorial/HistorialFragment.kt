011b6b7e6e7cb5b737177a77f9955769c7e54aee
BenjaminIsmael <tusisabesja12345@gmail.com>
2025-08-08 03:50:54 -0300

    Preparando proyecto, con clean arquitecture y Navegation Component listo

---
+++ app/src/main/java/com/benjamin/proyectofeedo/UI/feedoHistorial/HistorialFragment.kt
@@ -0,0 +1,24 @@
+package com.benjamin.proyectofeedo.UI.feedoHistorial
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.benjamin.proyectofeedo.R
+import com.benjamin.proyectofeedo.databinding.FragmentHistorialBinding
+import com.benjamin.proyectofeedo.databinding.FragmentMenuBinding
+
+class HistorialFragment : Fragment() {
+
+    private var _binding: FragmentHistorialBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHistorialBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/benjamin/proyectofeedo/UI/feedoMenu/MenuFragment.kt
@@ -0,0 +1,23 @@
+package com.benjamin.proyectofeedo.UI.feedoMenu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.benjamin.proyectofeedo.R
+import com.benjamin.proyectofeedo.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMenuBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
