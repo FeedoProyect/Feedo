b9ec88c205e759bc45d44df61e4bbb1bb77af3ea
fariassantiago <fariassantiago071@gmail.com>
2025-09-01 00:49:57 -0300

    modo oscuro en detalle y perfilpersonalizable

--- app/src/main/java/com/benjamin/proyectofeedo/pantallasPrincipales/UI/feedoPerfil/EditarPerfilFragment.kt
+++ app/src/main/java/com/benjamin/proyectofeedo/pantallasPrincipales/UI/feedoPerfil/EditarPerfilFragment.kt
@@ -1,4 +1,34 @@
 package com.benjamin.proyectofeedo.pantallasPrincipales.UI.feedoPerfil
 
-class EditarPerfilFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.benjamin.proyectofeedo.databinding.FragmentEditarPerfilBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EditarPerfilFragment : Fragment() {
+
+    private var _binding: FragmentEditarPerfilBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSave.setOnClickListener {
+
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEditarPerfilBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
