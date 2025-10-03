dacae2d0b018f48c15d419a6117653dd83b835fb
BenjaminIsmael <tusisabesja12345@gmail.com>
2025-10-03 00:45:05 -0300

    Pantalla de Añadir Recetas terminada, solo queda el backend ahora, ahora la imagen de invitaciona añadir foto de la comida depende su visibilidad del modo oscuro y claro, por eso cambiamos el FeedoApp, arreglando tambien el bug del switch de settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt")
     id("androidx.navigation.safeargs.kotlin")
     kotlin("plugin.serialization") version "2.2.0"
-
+    id("kotlin-parcelize")
 }
 
 android {
@@ -86,6 +86,7 @@ android {
 
         implementation(platform("io.github.jan-tennert.supabase:bom:$supabase_version"))
         implementation("io.github.jan-tennert.supabase:postgrest-kt")
+        implementation("io.github.jan-tennert.supabase:storage-kt")
         implementation("io.ktor:ktor-client-android:$ktor_version")
 
 
